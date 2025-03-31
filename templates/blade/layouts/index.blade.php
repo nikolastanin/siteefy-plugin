b7fdd9f834f7d4fd523a655f18a222d05ca65eb9
Nikola Stanin <nikolastanin@gmail.com>
2025-03-31 13:30:43 +0200

    fix

--- templates/blade/layouts/index.blade.php
+++ templates/blade/layouts/index.blade.php
@@ -8,8 +8,8 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inria+Sans:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
-    {{wp_enqueue_style('main-siteefy-style-new')}}
     <?php wp_head();?>
+    {{wp_enqueue_style('main-siteefy-style-new')}}
 </head>
 <body>
 @include('nav')
