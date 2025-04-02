2b7576b83d6f72e217483da4a81d56b2e81156c1
Nikola Stanin <nikolastanin@gmail.com>
2025-04-02 09:43:15 +0200

    fixes

--- templates/footer.php
+++ templates/footer.php
@@ -2,11 +2,13 @@
 
 $element_id = 31719; // Replace with your Hook Element's ID
 $element = get_post($element_id);
-
+echo '<footer>';
 if ($element) {
     echo apply_filters('the_content', $element->post_content);
-}
+};
+echo '</footer>';
 ?>
+
 <style>
     @media (max-width: 1024px) {
         .gb-grid-wrapper.gb-grid-wrapper-8ebb59b1 {
