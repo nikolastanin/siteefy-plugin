20d3af078ed34b4a841ff2ddd28860dd0a94679d
Nikola Stanin <nikolastanin@gmail.com>
2025-06-13 18:30:39 +0200

    fix-a-href

--- blade.php
+++ blade.php
@@ -53,7 +53,7 @@ class Siteefy {
         if($env === 'dev'){
             return time();
         }else{
-            return '1.72';
+            return '1.73';
 //            return time();
         }
     }
