3b860c94c3ab72875e402cc2b20456d844fb15d8
Nikola Stanin <nikolastanin@gmail.com>
2025-06-25 14:56:50 +0200

    debug

--- index.php
+++ index.php
@@ -561,7 +561,7 @@ function siteefy_add_custom_templates($template) {
         ]);
         exit;
     }elseif(is_archive() && get_post_type($post) === 'task' && get_queried_object()->taxonomy !=='solution'){
-        $tasks = get_all_tasks(-1);
+        $tasks = array();
         $categories = get_all_categories(5);
         $tool_of_the_week = get_selected_tool_of_the_week();
         $count_of_items = count($tasks);
